5ac75985193df782e6d97bbc317ee4ab9185e5ce
Bryan Terce <bryan.terce@appfolio.com>
2020-06-24 15:19:23 -0700

    bt - generate controller for images

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+  test 'has a link to the image submission form' do
+    get root_url
+    assert_response :success
+    assert_select "a[href='#{new_image_path}']"
+  end
+end
