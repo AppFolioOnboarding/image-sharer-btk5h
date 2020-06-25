6d0e4c202834160d5faa2adf6831088cbb5e3fc7
Bryan Terce <bryan.terce@appfolio.com>
2020-06-24 23:26:47 -0700

    bt - add endpoint for submitting images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,21 @@
 class ImagesController < ApplicationController
   def new; end
+
+  def create
+    @image = Image.new(image_params)
+
+    if @image.save
+      redirect_to @image
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:url)
+  end
 end
