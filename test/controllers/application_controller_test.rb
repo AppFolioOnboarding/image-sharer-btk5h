d27e17c804b626a47b4e08c5df7f50e9947ff083
Bryan Terce <bryan.terce@appfolio.com>
2020-06-25 14:48:57 -0700

    bt - make images link to the individual show page

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -14,8 +14,12 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
 
     get root_url
 
-    assert_select 'img[src=?]', first_image.url
-    assert_select 'img[src=?]', second_image.url
+    assert_select 'a[href=?]', image_path(first_image) do
+      assert_select 'img[src=?]', first_image.url
+    end
+    assert_select 'a[href=?]', image_path(second_image) do
+      assert_select 'img[src=?]', second_image.url
+    end
 
     assert body.index(second_image.url) < body.index(first_image.url),
            'The most recent image should appear first'
