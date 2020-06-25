f06593e2f4cf80c3feaffdf57bb7b4b10d443642
Bryan Terce <bryan.terce@appfolio.com>
2020-06-25 14:29:13 -0700

    bt - add index page tests

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -6,4 +6,18 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'a[href=?]', new_image_path
   end
+
+  test 'displays newer images first' do
+    Image.delete_all
+    first_image = Image.create!(url: 'http://example.com/foo.png')
+    second_image = Image.create!(url: 'http://example.com/bar.png')
+
+    get root_url
+
+    assert_select 'img[src=?]', first_image.url
+    assert_select 'img[src=?]', second_image.url
+
+    assert body.index(second_image.url) < body.index(first_image.url),
+           'The most recent image should appear first'
+  end
 end
