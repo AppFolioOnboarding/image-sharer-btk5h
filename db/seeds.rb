24fb2b56e16fb83f58ea9abced9e9f046aa2bb92
Bryan Terce <bryan.terce@appfolio.com>
2020-06-26 14:26:41 -0700

    bt - add tags to database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
 image_seeds = [
-  'https://images.dog.ceo/breeds/terrier-norfolk/n02094114_1330.jpg',
-  'https://images.dog.ceo/breeds/spaniel-blenheim/n02086646_127.jpg',
-  'https://images.dog.ceo/breeds/shiba/shiba-10.jpg',
-  'https://images.dog.ceo/breeds/dhole/n02115913_312.jpg',
-  'https://images.dog.ceo/breeds/chow/n02112137_7668.jpg',
-  'https://images.dog.ceo/breeds/chow/n02112137_16109.jpg',
-  'https://avatars.githubusercontent.com/u/15284810',
-  'https://images.dog.ceo/breeds/shiba/shiba-6.jpg',
-  'https://images.dog.ceo/breeds/mountain-bernese/n02107683_4386.jpg',
-  'https://images.dog.ceo/breeds/pug/n02110958_6966.jpg',
-  'https://images.dog.ceo/breeds/dhole/n02115913_139.jpg',
-  'https://images.dog.ceo/breeds/terrier-fox/n02095314_777.jpg',
-  'https://images.dog.ceo/breeds/hound-afghan/n02088094_13145.jpg',
-  'https://images.dog.ceo/breeds/hound-afghan/n02088094_13270.jpg',
-  'https://images.dog.ceo/breeds/greyhound-italian/n02091032_1743.jpg',
-  'https://images.dog.ceo/breeds/elkhound-norwegian/n02091467_6920.jpg',
-  'https://images.dog.ceo/breeds/deerhound-scottish/n02092002_4230.jpg',
-  'https://images.dog.ceo/breeds/boxer/n02108089_1560.jpg',
-  'https://images.dog.ceo/breeds/setter-irish/n02100877_8321.jpg'
+  ['https://images.dog.ceo/breeds/terrier-norfolk/n02094114_1330.jpg', ['norfolk terrier']],
+  ['https://images.dog.ceo/breeds/spaniel-blenheim/n02086646_127.jpg', ['blenheim spaniel', 'humans???']],
+  ['https://images.dog.ceo/breeds/shiba/shiba-10.jpg', ['shiba inu']],
+  ['https://images.dog.ceo/breeds/dhole/n02115913_312.jpg', ['dhole']],
+  ['https://images.dog.ceo/breeds/chow/n02112137_7668.jpg', ['chow']],
+  ['https://images.dog.ceo/breeds/chow/n02112137_16109.jpg', ['chow']],
+  ['https://avatars.githubusercontent.com/u/15284810', ['chow', 'humans???']],
+  ['https://images.dog.ceo/breeds/shiba/shiba-6.jpg', ['shiba inu']],
+  ['https://images.dog.ceo/breeds/mountain-bernese/n02107683_4386.jpg', ['bernese mountain']],
+  ['https://images.dog.ceo/breeds/pug/n02110958_6966.jpg', ['pug']],
+  ['https://images.dog.ceo/breeds/dhole/n02115913_139.jpg', ['dhole']],
+  ['https://images.dog.ceo/breeds/terrier-fox/n02095314_777.jpg', ['fox terrier', 'humans???']],
+  ['https://images.dog.ceo/breeds/hound-afghan/n02088094_13145.jpg', ['afghan hound']],
+  ['https://images.dog.ceo/breeds/hound-afghan/n02088094_13270.jpg', ['afghan hound']],
+  ['https://images.dog.ceo/breeds/greyhound-italian/n02091032_1743.jpg', ['italian greyhound', 'humans???']],
+  ['https://images.dog.ceo/breeds/elkhound-norwegian/n02091467_6920.jpg', ['norwegian elkhound']],
+  ['https://images.dog.ceo/breeds/deerhound-scottish/n02092002_4230.jpg', ['scottish deerhound', 'humans???']],
+  ['https://images.dog.ceo/breeds/boxer/n02108089_1560.jpg', ['boxer']],
+  ['https://images.dog.ceo/breeds/setter-irish/n02100877_8321.jpg', ['irish setter', 'humans???']]
 ]
 
-image_seeds.each do |url|
-  Image.create!(url: url)
+image_seeds.each do |url, tag_list|
+  Image.create!(url: url, tag_list: tag_list)
 end
